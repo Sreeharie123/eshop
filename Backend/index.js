40479ee7baf96c5476530c5c5d227fecb5afd50b
Sreeharie123 <sreehariengoli@gmail.com>
2023-04-25 15:10:54 +0530

    updated the products

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,17 @@
 //import the express library
 const express = require("express");
 const app = express();
-//import the mongoose library
 const mongoose = require("mongoose");
-//import the dotenv library
+
 const dotenv = require('dotenv');
 const userCreate=require('./routes/createUser')
+const product=require('./routes/product')
+
+
 //config dotenv
 dotenv.config()
-//Connect the databas
+
+app.use(express.json())
 mongoose
   .connect(process.env.URL)
   .then((data) => {
@@ -17,10 +20,16 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+  
+  app.use('/user',userCreate)
+  app.use('/product',product)
+  
+
+
+
 //listening the port 4000
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server running on port 5000 ");
 });
 
-app.use(express.json())
-app.use('/user',userCreate)
